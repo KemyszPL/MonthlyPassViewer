b9eb62dd40203b48a83ac12c401245c07eb93ce6
KemyszPL <malpkaszymix@gmail.com>
2024-12-25 21:21:52 +0100

    copy img into temp app cache instead of perm. hotlink uri

--- app/src/main/java/pl/kroljulian/miesieczny/MainActivity.java
+++ app/src/main/java/pl/kroljulian/miesieczny/MainActivity.java
@@ -4,12 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     ImageView ticketImageView;
     //expected result code to compare actual result code to
     int SELECT_PICTURE = 200;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,27 +50,95 @@ public class MainActivity extends AppCompatActivity {
         ticketImageView.setImageURI(Uri.parse(mString));
     }
 
-    void imageChooser() {
-
+    public void imageChooser() {
         Intent i = new Intent();
         i.setType("image/*");
         i.setAction(Intent.ACTION_GET_CONTENT);
-
         startActivityForResult(Intent.createChooser(i, "Select picture"), SELECT_PICTURE);
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        //TODO: enable cropping of picture while selecting
+
         if(resultCode == RESULT_OK) {
             if (requestCode == SELECT_PICTURE) {
                 Uri selectedImageUri = data.getData();
-                SharedPreferences mPrefs = getSharedPreferences("label", 0);
-                SharedPreferences.Editor mEditor = mPrefs.edit();
-                mEditor.putString("imageUriVariable", String.valueOf(selectedImageUri)).commit();
-                if (null != selectedImageUri) {
-                    ticketImageView.setImageURI(selectedImageUri);
+                if (selectedImageUri != null) {
+                    // Copy the image to the app's cache directory
+                    File copiedFile = copyImageToCache(selectedImageUri);
+
+                    if (copiedFile != null) {
+                        // Save the path of the copied file to SharedPreferences
+                        SharedPreferences mPrefs = getSharedPreferences("label", 0);
+                        SharedPreferences.Editor mEditor = mPrefs.edit();
+                        mEditor.putString("imageFilePath", copiedFile.getAbsolutePath()).apply();
+
+                        // Load the image into the ImageView
+                        ticketImageView.setImageURI(Uri.fromFile(copiedFile));
+                    }
                 }
+            }
+        }
+    }
+
+    // Method to copy the image from the URI to the app's cache directory
+    private File copyImageToCache(Uri uri) {
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        File cachedImageFile = null;
+
+        try {
+            // Get input stream for the URI
+            inputStream = getContentResolver().openInputStream(uri);
+
+            // Create a file in the cache directory
+            cachedImageFile = new File(getCacheDir(), "cached_image_" + System.currentTimeMillis() + ".jpg");
+
+            // Create an output stream to write to the cache file
+            outputStream = new FileOutputStream(cachedImageFile);
+
+            // Copy the input stream to the output stream
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, length);
+            }
+
+            // Flush and close the streams
+            outputStream.flush();
+
+            return cachedImageFile;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (inputStream != null) inputStream.close();
+                if (outputStream != null) outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Load the image from SharedPreferences when the app resumes
+        SharedPreferences mPrefs = getSharedPreferences("label", 0);
+        String imagePath = mPrefs.getString("imageFilePath", null);
+
+        if (imagePath != null) {
+            File imgFile = new File(imagePath);
+            if (imgFile.exists()) {
+                // Set the image from the cached file
+                ticketImageView.setImageURI(Uri.fromFile(imgFile));
+            } else {
+                // If the file doesn't exist, log it and set a placeholder
+                Log.e("ImageChooser", "Image file doesn't exist when resuming");
+                ticketImageView.setImageResource(R.drawable.ic_launcher_background);  // Placeholder image
             }
         }
     }
