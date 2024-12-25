c0c79c6d8b57395328b82cdf685a47876f2b6b46
KemyszPL <malpkaszymix@gmail.com>
2024-12-25 21:25:58 +0100

    remove unneeded imports

--- app/src/main/java/pl/kroljulian/miesieczny/MainActivity.java
+++ app/src/main/java/pl/kroljulian/miesieczny/MainActivity.java
@@ -4,15 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
